ea3ae8a214e89c7aded1165420021155c4ef5e17
Kathryn Reese <kathryn.ivyreese@gmail.com>
2024-06-10 18:44:35 -0600

    more css and html updates

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,21 +1,21 @@
 
 function buildBarChart(chartData) {
-  
+
     let groupedCrimes = chartData.reduce((acc, str) => {
         if (!acc[str]) {
-          acc[str] = 0;
+            acc[str] = 0;
         }
         acc[str]++;
         return acc;
-      }, {});
+    }, {});
 
     let labels = Object.keys(groupedCrimes);
     let counts = Object.values(groupedCrimes);
 
     let index = labels.indexOf("ASSAULT W/DANGEROUS WEAPON");
-        if (index !== -1) {
+    if (index !== -1) {
         labels[index] = "ASSAULT";
-        }
+    }
 
     var trace1 = {
         x: labels,
@@ -24,18 +24,18 @@ function buildBarChart(chartData) {
         type: 'bar'
         
     };
-    
+
     var data = [trace1];
-    
+
     var layout = {
         title: 'Amount of Crimes by Type',
         xaxis: {
             automargin: true,
         },
         yaxis: {
-          title: 'Number of Crimes'
+            title: 'Number of Crimes'
         }
-      };
-    
+    };
+
     Plotly.newPlot('myDiv', data, layout);
 }
